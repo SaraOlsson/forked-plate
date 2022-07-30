37a09a9a8c08502a30d2b3f9ee994a87cab5031a
Sara Olsson <sara@edgeimpulse.com>
2022-07-31 00:18:32 +0200

    adjust to PWA checklist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'logo192_c.png', 'logo512_c.png', 'maskable-dummy'],
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        "name": "ForkedPlate",
+        "short_name": "Forked Plate",
+        "description": "Your goto recipe platform",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "icons": [
+          {
+            src: "logo192_c.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "logo512_c.png",
+            type: "image/png",
+            sizes: "512x512"
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
